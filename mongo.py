2daf60d82b5e564ec3a2e6300396141512c4f67f
Ethan Steere <ethansteere1@gmail.com>
2022-06-01 17:18:44 -0400

    Mongo Progress

--- mongo.py
+++ mongo.py
@@ -1,4 +1,21 @@
-'''Mongo Queries'''
+from pymongo import MongoClient 
+from pymongo.errors import ConnectionFailure
+from decouple import config
+
+def connect_to_mongo():
+    '''Attempts to establish connect to mongo in EC2 
+    throws ConnectException if something goes wrong
+
+    return: mongo client
+    rtype: MongoClient '''
+    try: 
+        return MongoClient(config('IP'), 27017)
+    except ConnectionError:
+        print('Connection to MongoDB failed, check connection information and try again') 
+
 
 def _mongo_get_meals(ingr_list):
-    pass 
+    client = connect_to_mongo()
+    collection = client.project2.food
+    # QUERY GOES HERE
+    # return answer
