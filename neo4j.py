7bb6e20737586c01000da4b5dcecc4d3c0ac3bfb
Ethan Steere <ethansteere1@gmail.com>
2022-06-01 17:25:44 -0400

    neo progress

--- neo4j.py
+++ neo4j.py
@@ -1,4 +1,15 @@
-'''Neo4j Queries'''
+from py2neo import Graph
+from decouple import config
+
+def connect_to_neo():
+    '''Attempts to establish connect to Neo4j in EC2 
+
+    return: Graph Object 
+    rtype: py2neo.database.Graph '''
+    try: 
+        return Graph("bolt://{}:7687".fornat(config('IP')), auth = (config('NEO_USERNAME'), config("NEO_PASSWORD")))
+    except:
+        print('Connection to Neo4j failed, check connection information and try again')
 
 def _neo4j_get_meals(ingr_list):
-    pass
+    client = connect_to_neo()
